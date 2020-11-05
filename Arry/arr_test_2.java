2881da561adece6063517886a2ae3d92a93bc56a
wangjiade <wangjiade@oppo.com>
2020-11-05 18:02:46 +0800

    add some code for ele

---
+++ Arry/arr_test_2.java
@@ -0,0 +1,14 @@
+public class arr_test_2 {
+
+  public static void main(String[] args) {
+    int[] arr = new int[4];
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+
+    char[] arr1 = new char[4];
+    for (int i = 0; i < arr1.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+}
